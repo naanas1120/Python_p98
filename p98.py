14e70ff318740f64b7176fcb39a94c4d6a786249
naanas1120 <74460655+naanas1120@users.noreply.github.com>
2022-02-09 23:35:51 +0530

    Add files via upload

---
+++ p98.py
@@ -0,0 +1,17 @@
+def swapFileData():
+    file1=input("entere the file1: ")
+    file2=input("enter the file2: ")
+    
+    with open(file1,'r') as a:
+        data_a=a.read()
+    
+    with open(file2,'r') as b:
+        data_b=b.read()
+
+    with open(file1,'w') as a:
+        a.write(data_b)
+    
+    with open(file2,'w') as b:
+        b.write(data_a)
+
+swapFileData()
